2b175704f0846b7b52e535f5bfa87b4a076480f3
Gxy-2001 <njuguanxinyu@foxmail.com>
2021-02-17 11:19:08 +0800

    2.17

--- src/每日一题/Solution0566.java
+++ src/每日一题/Solution0566.java
@@ -7,4 +7,18 @@ package 每日一题;
  * @create: 2021-02-17
  */
 public class Solution0566 {
+    public int[][] matrixReshape(int[][] nums, int r, int c) {
+        if (nums[0].length * nums.length != r * c) {
+            return nums;
+        }
+        int[][] res = new int[r][c];
+        int t = 0, h = nums[0].length;
+        for (int i = 0; i < r; i++) {
+            for (int j = 0; j < c; j++) {
+                res[i][j] = nums[t / h][t % h];
+                t++;
+            }
+        }
+        return res;
+    }
 }
